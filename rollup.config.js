a4a6c0e62f929166047b32c3677ccf0895d86980
Ryan Timmons <ryan.timmons@10gen.com>
2019-06-03 14:09:03 -0400

    whitespace

--- rollup.config.js
+++ rollup.config.js
@@ -3,16 +3,18 @@ import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript';
 
 module.exports = {
-	input: 'index.ts',
-	plugins: [
-		typescript() // so Rollup can convert TypeScript to JavaScript
-	],
-	output: [
-		{     file: 'rolled-up.js',
-    format: 'iife',
-    name: 'rolledUp',
-    footer: ';rolledUp();',
-    sourcemap: true, },
-	]
+    input: 'index.ts',
+    plugins: [
+        typescript() // so Rollup can convert TypeScript to JavaScript
+    ],
+    output: [
+        {
+            file: 'rolled-up.js',
+            format: 'iife',
+            name: 'rolledUp',
+            footer: ';rolledUp();',
+            sourcemap: true,
+        },
+    ]
 };
 
